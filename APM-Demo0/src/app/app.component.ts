e919829ae45986a5ffac9d5333787a2003d02682
sathish <sathish@.com>
2020-05-04 07:37:34 -0500

    Update app.component.ts
    my followups

--- APM-Demo0/src/app/app.component.ts
+++ APM-Demo0/src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {from, of} from 'rxjs';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    of('Apple', 'Fiji').subscribe(console.log);
+    from(['myple', 'fuji']).
+    subscribe(
+      item => console.log(),
+      err => console.error('error occured ${err}'),
+      () => console.log('No more apples')
+    );
+  }
+}
